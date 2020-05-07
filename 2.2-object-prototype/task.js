41e9a10f3cfd4c4b6092b474ea512352b7389606
Mtaka <mary.mtaka@gmail.com>
2020-05-07 23:44:40 +0500

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,35 @@
+'use strict';
+
 function getAnimalSound(animal) {
     // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) {
+        return null;
+    }
+    let sound = animal.sound;
+    return sound;
 }
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    }
+    let average = 0;
+    for (let i = 0; i < marks.length; i++) {
+        average += parseInt(marks[i]);
+    }
+    average = average / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
-    // return verdict
+    let now = +new Date;
+    birthday = +new Date(birthday);
+    let diff = now - birthday;
+    let mmInYear = (3*365 + 366) * 24 * 60 * 60 * 1000 / 4;
+    let age = diff / mmInYear;
+    let verdict = age >= 18 ? true : false;
+    return verdict
 }
